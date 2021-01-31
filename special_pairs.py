25e44d345b980d0dea55060e03409d6ff48e858d
Pete Houghton <pete@investigatingsoftware.co.uk>
2021-01-31 13:29:24 +0000

    Added a few test cases

--- special_pairs.py
+++ special_pairs.py
@@ -15,15 +15,24 @@ def special_pairs(nums):
         else:
             match_store[current_num] = {index: True}
 
-    print(match_store)
     for indexes in match_store.values():
         index_keys = list(indexes.keys())
         for pos_a in range(0, len(index_keys)):
             for pos_b in range(pos_a+1, len(index_keys)):
                 specials.append((index_keys[pos_a], index_keys[pos_b]))
 
-    return specials
+    return len(specials), specials
 
 print(special_pairs([1,2,3,1,1,3]))
 print()
 print(special_pairs([1,2,3,1,1,3,2]))
+print()
+print(special_pairs([1,2,2]))
+print()
+print(special_pairs([1,1,1,1]))
+print()
+print(special_pairs([1]))
+print()
+print(special_pairs([111,111]))
+print()
+print(special_pairs([1,2,3,4,4,3,2,1]))
